dc1584b1cc70a444f24378495239646b88d489d8
ggom121@naver.com <ggom121@naver.com>
2023-09-08 11:12:51 +0900

    19: 오류 페이지와 홈페이지 구현

--- Node.js/main.js
+++ Node.js/main.js
@@ -5,37 +5,63 @@ const url = require('url');
 const app = http.createServer(function(request, response){
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
-    let title = queryData.id;
-    if(_url == '/'){
-        title = 'Welcome';
+    let pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            let title = 'Welcome';
+            var description = 'Hello, Node.js';
+            let template = `
+            <!doctype html>
+            <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+        } else{
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                let title = queryData.id;
+                let template = `
+                <!doctype html>
+                <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ul>
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-        let template = `
-        <!doctype html>
-        <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-        </html>
-        `;
-        response.end(template);
-    });
 });
 
 app.listen(3000);
