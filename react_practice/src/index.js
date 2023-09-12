1c3a4925ae9b8e273f76aa627857a402d3490c43
ggom121@naver.com <ggom121@naver.com>
2023-09-12 23:32:26 +0900

    10장: 리스트와 키

--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -7,15 +7,16 @@ import reportWebVitals from './reportWebVitals';
 import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
 import CommentList from './chapter_05/CommentList';
-import NotificationList from './chpater_06/NotificationList';
+import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <AttendanceBook />
   </React.StrictMode>
 );
 
