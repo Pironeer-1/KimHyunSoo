d57053ed3b6acea7b438358597238844716a0db2
ggom121@naver.com <ggom121@naver.com>
2023-09-15 13:45:58 +0900

    35: 글 수정(수정할 정보 전송)

--- Node.js/main.js
+++ Node.js/main.js
@@ -69,7 +69,7 @@ const app = http.createServer(function(request, response){
             let title = 'WEB - create';
             let list = templateList(filelist);
             let template = templateHTML(title, list, `
-                <form action="http://localhost:3000/create_process" method="post">
+                <form action="/create_process" method="post">
                     <p><input type="text" name="title" placeholder="title"></p>
                     <p>
                         <textarea name="description" placeholder="description"></textarea>
@@ -94,6 +94,31 @@ const app = http.createServer(function(request, response){
             fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
                 response.writeHead(302, {Location: `/?id=${title}`});
                 response.end();
+            });
+        });
+    } else if(pathname === '/update'){
+        fs.readdir('./data', function(error, filelist){
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                let title = queryData.id;
+                let list = templateList(filelist);
+                let template = templateHTML(title, list, 
+                    `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `,
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                );
+                response.writeHead(200);
+                response.end(template);
             });
         });
     } else {
