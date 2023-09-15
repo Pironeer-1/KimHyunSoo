99f4841bb19d029bc482a648eccab3ba2985d384
ggom121@naver.com <ggom121@naver.com>
2023-09-15 13:18:57 +0900

    34: 글 수정(수정 링크 생성)

--- Node.js/main.js
+++ Node.js/main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
     return `
     <!doctype html>
     <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
         <body>
             <h1><a href="/">WEB2</a></h1>
             ${list}
-            <a href="/create">create</a>
+            ${control}
             ${body}
         </body>
     </html>
@@ -42,7 +42,10 @@ const app = http.createServer(function(request, response){
                 let title = 'Welcome';
                 let description = 'Hello, Node.js';
                 let list = templateList(filelist);
-                let template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                let template = templateHTML(title, list, 
+                    `<h2>${title}</h2><p>${description}</p>`,
+                    `<a href="/create">create</a>`
+                );
                 response.writeHead(200);
                 response.end(template);
             })
@@ -52,7 +55,10 @@ const app = http.createServer(function(request, response){
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                     let title = queryData.id;
                     let list = templateList(filelist);
-                    let template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                    let template = templateHTML(title, list, 
+                        `<h2>${title}</h2><p>${description}</p>`,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                    );
                     response.writeHead(200);
                     response.end(template);
                 });
@@ -72,7 +78,7 @@ const app = http.createServer(function(request, response){
                     <input type="submit">
                     </p>
                 </form>
-            `);
+            `,'');
             response.writeHead(200);
             response.end(template);
         });
