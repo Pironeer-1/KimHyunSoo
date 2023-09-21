c5e9e664b67f762ee9a5610a1cec4661bf88cfe5
ggom121@naver.com <ggom121@naver.com>
2023-09-21 16:51:43 +0900

    MySQL 06: mysql을 이용한 글 생성 기능 구현

--- 02_nodejs-mysql/main.js
+++ 02_nodejs-mysql/main.js
@@ -61,9 +61,9 @@ var app = http.createServer(function(request, response) {
             });
         }
     } else if(pathname === '/create') {
-        fs.readdir('./data', function(error, filelist) {
-            var title = 'WEB - create';
-            var list = template.list(filelist);
+    db.query(`SELECT * FROM topic`, function(error, topics){
+            var title = 'Create';
+            var list = template.list(topics);
             var html = template.HTML(title, list, `
                 <form action="/create_process" method="post">
                     <p><input type="text" name="title" placeholder="title"></p>
@@ -74,7 +74,7 @@ var app = http.createServer(function(request, response) {
                         <input type="submit">
                     </p>
                 </form>
-            `, '');
+            `, `<a href="/create">create</a>`);
             response.writeHead(200);
             response.end(html);
         });
@@ -85,12 +85,18 @@ var app = http.createServer(function(request, response) {
         });
         request.on('end', function() {
             var post = qs.parse(body);
-            var title = post.title;
-            var description = post.description;
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-                response.writeHead(302, {Location: `/?id=${title}`});
-                response.end();
-            });
+            db.query(`
+                INSERT INTO topic (title, description, created, author_id) 
+                    VALUES(?, ?, NOW(), ?)`,
+                [post.title, post.description, 1],
+                function(error, result){
+                    if(error){
+                        throw error;
+                    }
+                    response.writeHead(302, {Location: `/?id=${result.insertId}`});
+                    response.end();
+                }
+            );
         });
     } else if(pathname === '/update') {
         fs.readdir('./data', function(error, filelist) {
