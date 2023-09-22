bc24c8c1783ef920f4aa53393c39423fe5474afe
ggom121@naver.com <ggom121@naver.com>
2023-09-22 23:46:16 +0900

    MySQL 20: 보안:SQL 인젝션

--- 02_nodejs-mysql/lib/topic.js
+++ 02_nodejs-mysql/lib/topic.js
@@ -23,7 +23,7 @@ exports.page = function(request, response){
         if(error) {
             throw error;
         }
-        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function(error2, topic){
+        let query = db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function(error2, topic){
             if(error2){
                 throw error2;
             }
